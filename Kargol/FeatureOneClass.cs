d4ef892d74fcff7e850136109fd0e4d23496c23b
Marcin Angielczyk <ma853722@wmservice.corpnet1.com>
2018-11-21 13:50:24 +0100

    Added FeatureOneClass class.

---
+++ Kargol/FeatureOneClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class FeatureOneClass
+{
+    public int MyProperty { get; set; }
+
+    public string MyMethod()
+    {
+        return "string";
+    }
+
+    public string MyOtherMethod() => "string";
+}
